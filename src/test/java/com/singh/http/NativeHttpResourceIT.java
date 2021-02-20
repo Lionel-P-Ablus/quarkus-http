d79547110b534eefca32a1f0cdc8d6e0ada7493c
Praneed.K <>
2021-02-20 17:10:46 +0700

    [init] quarkus http

---
+++ src/test/java/com/singh/http/NativeHttpResourceIT.java
@@ -0,0 +1,9 @@
+package com.singh.http;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHttpResourceIT extends HttpResourceTest {
+
+    // Execute the same tests but in native mode.
+}
